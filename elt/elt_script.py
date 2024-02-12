d6f7b6cd8ba746b50df088df5c2598cbaedcff3c
e-ian <ianemma70@gmail.com>
2024-02-12 19:33:58 +0300

    - add load command for destination db

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -9,7 +9,7 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
                 ["pg_isready", "-h", host], check=True, capture_output=True, text=True
             )
             if "accepting connections" in result.stdout:
-                print("Successfully connected to postgres")
+                print("Successfully connected to PostgreSQL")
                 return True
         except subprocess.CalledProcessError as e:
             print(f"Error connecting to postgres: {e}")
@@ -27,29 +27,50 @@ if not wait_for_postgres(host="source_postgres"):
 
 print("Starting ELT Script...")
 
+# Configuration for the source PostgreSQL database
 source_config = {
     'dbname':'source_db',
     'user': 'postgres',
     'password': 'secret',
-    'host': 'source_db'
+    'host': 'source_postgres'
 }
 
+# Configuration for the destination PostgreSQL database
 destination_config = {
     'dbname':'destination_db',
     'user': 'postgres',
     'password': 'secret',
-    'host': 'destination_db'
+    'host': 'destination_postgres'
 }
 
 dump_command = [
     'pg_dump',
     '-h', source_config['host'],
-    '-u', source_config['user'],
+    '-U', source_config['user'],
     '-d', source_config['dbname'],
     '-f', 'data_dump.sql',
-    '-w'
+    '-w' # do not prompt for password
 ]
 
+# set the PGPASSWORD environmnet variable to avoid password prompt
 subprocess_env = dict(PGPASSWORD=source_config['password'])
 
+# execute the dump command
 subprocess.run(dump_command, env=subprocess_env, check=True)
+
+# Use psql to load the dumped SQL file into the destination database
+load_command = [
+    'psql',
+    '-h', destination_config['host'],
+    '-U', destination_config['user'],
+    '-d', destination_config['dbname'],
+    '-a', '-f' 'data_dump.sql'
+]
+
+# Set the PGPASSWORD environment variable for the destination database
+subprocess_env = dict(PGPASSWORD=destination_config['password'])
+
+# execute the load command
+subprocess.run(load_command, env=subprocess_env, check=True)
+
+print("Ending ELT Script...")
